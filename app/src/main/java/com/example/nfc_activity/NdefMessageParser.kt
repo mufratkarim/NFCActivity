0cdfa340095b407a72a79dfdacd3433f1a9bc8e4
Mufrat Karim Aritra <mufratkarimaritra@gmail.com>
2023-11-01 21:06:14 -0400

    Created GitHub Workflows

--- app/src/main/java/com/example/nfc_activity/NdefMessageParser.kt
+++ app/src/main/java/com/example/nfc_activity/NdefMessageParser.kt
@@ -24,25 +24,30 @@ object NdefMessageParser {
     fun getRecords(records: Array<NdefRecord>): List<ParsedNdefRecord> {
         val elements = mutableListOf<ParsedNdefRecord>()
         for (record in records) {
-            if (UriRecord.isUri(record)) {
-                elements.add(UriRecord.parse(record))
-            } else if (TextRecord.isText(record)) {
-                elements.add(TextRecord.parse(record))
-            } else if (SmartPoster.isPoster(record)) {
-                elements.add(SmartPoster.parse(record))
-            } else {
-                elements.add(object : ParsedNdefRecord {
-                    override fun getView(
-                        activity: Activity,
-                        inflater: LayoutInflater,
-                        parent: ViewGroup,
-                        offset: Int
-                    ): View {
-                        val text = inflater.inflate(R.layout.tag_text, parent, false) as TextView
-                        text.text = String(record.payload)
-                        return text
-                    }
-                })
+            when {
+                UriRecord.isUri(record) -> {
+                    elements.add(UriRecord.parse(record))
+                }
+                TextRecord.isText(record) -> {
+                    elements.add(TextRecord.parse(record))
+                }
+                SmartPoster.isPoster(record) -> {
+                    elements.add(SmartPoster.parse(record))
+                }
+                else -> {
+                    elements.add(object : ParsedNdefRecord {
+                        override fun getView(
+                            activity: Activity,
+                            inflater: LayoutInflater,
+                            parent: ViewGroup,
+                            offset: Int
+                        ): View {
+                            val text = inflater.inflate(R.layout.tag_text, parent, false) as TextView
+                            text.text = String(record.payload)
+                            return text
+                        }
+                    })
+                }
             }
         }
         return elements
